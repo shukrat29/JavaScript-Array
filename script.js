7a3a14bdb3af995ce03fc7448ca394dee24c9a88
shukra <shukratamang.npl@gmail.com>
2024-06-02 21:24:13 +1000

    different types of array method

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ const val = [
   () => {},
 ];
 console.log(val);
+
+const fruits = ['banana', 'apple', 'orange', 'mango'];
+console.log(fruits);
+console.log(fruits.length);
+const value = fruits[3];
+console.log(value);
+
+// adding data at the end
+// push
+fruits.push('watermelon');
+console.log(fruits);
+
+// adding item at the start of an array
+fruits.unshift('peach');
+console.log(fruits);
+
+// adding item somewhere in between
+// splice
+fruits.splice(2, 0, 'kaaju');
+console.log(fruits);
+
+// deleting items
+// deleting items from the end of an array
+fruits.pop();
+console.log(fruits);
